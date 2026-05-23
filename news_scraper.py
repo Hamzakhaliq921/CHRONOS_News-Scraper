e4f92e57eb25dc667a53e12e6e9b530a745c666a
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2026-05-23 15:14:02 -0600

    NewsAggregator

--- news_scraper.py
+++ news_scraper.py
@@ -6,6 +6,21 @@ import time
 import os
 
 
+class NewsAggregator:
+    def __init__(self):
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--window-size=1920,1080')
+        chrome_options.add_argument(
+            'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+            'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        )
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.all_news = {}
+
     # ------------------------------------------------------------------
     # BBC News
     # ------------------------------------------------------------------
